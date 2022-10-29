4e2f932da63638e38bfce90f6c8e972bbab5f6cb
Youmin Kim <86945989+march0320@users.noreply.github.com>
2022-10-30 01:39:00 +0900

    [투 포인터] 10월 30일

---
+++ 10월 11일 - 투 포인터/6159.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int main() {
+    int n, s, cnt=0;
+    //입력
+    cin >> n >> s;
+    vector<int> cow(n, 0);
+    for (int i = 0; i < n; i++) {
+        cin >> cow[i];
+    }
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = i+1; j < n; j++) {
+            if (cow[i] + cow[j] <= s)   //두마리의 소 크기의 합 < 코스튬 크기
+                cnt += 1;
+        }
+    }
+   
+    cout << cnt;
+
+    return 0;
+}
